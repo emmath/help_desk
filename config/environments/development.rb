ba89f70a3b8d13e0e9db0e95c89812571d927209
emmath <emmatrevena@gmail.com>
2013-08-07 17:26:43 -0700

    config update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,8 +21,8 @@ HelpDesk::Application.configure do
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :domain               => "gmail.com",
-    :user_name            => ENV["visibleemma"],
-    :password             => ENV["factoria"],
+    :user_name            => "visibleemma",
+    :password             => "factoria",
     :authentication       => "plain",
     :enable_starttls_auto => true
     }
